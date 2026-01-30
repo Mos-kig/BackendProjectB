7880833f4f51d7352e95ea188706478a4de97aed
Mos-kig <backtosweden61@hotmail.com>
2026-01-30 19:58:06 +0100

    Overview Friends by country page done

--- AppRazor/Pages/FriCou.cshtml.cs
+++ AppRazor/Pages/FriCou.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Models.Interfaces;
+using Services;
+using Services.Interfaces;
 
 namespace AppRazor.Pages
 {
     public class FriCouModel : PageModel
     {
-        public void OnGet()
+        public readonly IAdminService _adminService;
+        public IEnumerable<IGrouping<string, Models.DTO.GstUsrInfoFriendsDto>>? CountryInfo;
+
+        public async Task<IActionResult> OnGet()
         {
+            var info = await _adminService.GuestInfoAsync();
+            CountryInfo = info.Item.Friends.GroupBy(f => f.Country);
+            return Page();
+        }
+        public FriCouModel(IAdminService adminService)
+        {
+            _adminService = adminService;
         }
     }
 }
