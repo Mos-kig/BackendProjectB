a92623bdb7e521986f0e1519d13931ee4af0c45c
Mos-kig <backtosweden61@hotmail.com>
2026-01-31 13:26:23 +0100

    View the details of a Friend, including Address, Pets and Quotes

--- AppRazor/Pages/VDFAPQ.cshtml.cs
+++ AppRazor/Pages/VDFAPQ.cshtml.cs
@@ -1,12 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Models;
+using Services.Interfaces;
 
 namespace AppRazor.Pages
 {
     public class VDFAPQModel : PageModel
     {
-        public void OnGet()
+        readonly IFriendsService _service = null;
+        readonly ILogger<VDFAPQModel> _logger = null;
+        public csFriend Friend { get; set; }
+
+        public string ErrorMessage { get; set; } = null;
+
+        public async Task<IActionResult> OnGet(string id)
         {
+            try
+            {
+                Guid _id = Guid.Parse(id);
+                var response = await _service.ReadFriendAsync(_id, false);
+                Friend = response.Item as csFriend;
+            }
+            catch (Exception e)
+            {
+                ErrorMessage = e.Message;
+            }
+            return Page();
+        }
+        public VDFAPQModel(IFriendsService service, ILogger<VDFAPQModel> logger)
+        {
+            _logger = logger;
+            _service = service;
         }
     }
 }
