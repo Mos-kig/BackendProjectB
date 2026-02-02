199fb24f73d182883f97513a120da00ffc513bea
Mos-kig <backtosweden61@hotmail.com>
2026-02-02 09:57:49 +0100

    list of pets

--- AppRazor/Pages/FPCSC.cshtml.cs
+++ AppRazor/Pages/FPCSC.cshtml.cs
@@ -8,8 +8,10 @@ namespace AppRazor.Pages
     public class FPCSCModel : PageModel
     {
         public readonly IFriendsService _friendsService;
+        public readonly IPetsService _petsService;
 
         public IEnumerable<IGrouping<string, csFriend>>? CityInfo;
+        public IEnumerable<IGrouping<string, Pet>>? PetInfo;
 
         public async Task<IActionResult> OnGet()
         {
@@ -18,12 +20,19 @@ namespace AppRazor.Pages
 
             CityInfo = allFriends.GroupBy(f => f.Address?.City ?? "Unknown City")
                 .OrderBy(g => g.Key == "Unknown City" ? "zzz" : g.Key);
+
+            var petResponse = await _petsService.ReadPetsAsync(true, false, "", 0, int.MaxValue);
+            var allPets = petResponse.PageItems.Cast<Pet>().ToList();
+            PetInfo = allPets.GroupBy(p => p.Friend?.Address?.City ?? "Unknown City")
+                .OrderBy(g => g.Key == "Unknown City" ? "zzz" : g.Key);
+
             return Page();
         }
 
-        public FPCSCModel(IFriendsService friendsService)
+        public FPCSCModel(IFriendsService friendsService, IPetsService petsService)
         {
             _friendsService = friendsService;
+            _petsService = petsService;
         }
     }
 }
