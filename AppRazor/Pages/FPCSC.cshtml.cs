c92cd2e499fcc323e52322a8bd0d867d0b6e7154
Mos-kig <backtosweden61@hotmail.com>
2026-02-02 09:25:21 +0100

    city info

--- AppRazor/Pages/FPCSC.cshtml.cs
+++ AppRazor/Pages/FPCSC.cshtml.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Models;
+using Services.Interfaces;
 
 namespace AppRazor.Pages
 {
     public class FPCSCModel : PageModel
     {
-        public void OnGet()
+        public readonly IFriendsService _friendsService;
+
+        public IEnumerable<IGrouping<string, csFriend>>? CityInfo;
+
+        public async Task<IActionResult> OnGet()
         {
+            var response = await _friendsService.ReadFriendsAsync(true, false, "", 0, int.MaxValue);
+            var allFriends = response.PageItems.Cast<csFriend>().ToList();
+
+            CityInfo = allFriends.GroupBy(f => f.Address?.City ?? "Unknown City")
+                .OrderBy(g => g.Key == "Unknown City" ? "zzz" : g.Key);
+            return Page();
+        }
+
+        public FPCSCModel(IFriendsService friendsService)
+        {
+            _friendsService = friendsService;
         }
     }
 }
